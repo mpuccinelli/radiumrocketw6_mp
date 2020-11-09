671879dcb1b622a44fb89c6c3fafbda57a760ecd
Mauricio Puccinelli <puccinellimauricio@gmail.com>
2020-11-09 20:32:30 -0300

    02 Animating the canvas

--- game.js
+++ game.js
@@ -1,15 +1,32 @@
 var canvas = null,
     ctx = null;
+    x = 50;
+    y = 50;
 
 function paint(ctx) {
-    ctx.strokeStyle = '#000';
-    ctx.strokeRect(50, 50, 100, 60);
+    ctx.fillStyle = '#000';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = '#0f0';
+    ctx.fillRect(x, y, 10, 10);
 } 
 
 function init() {
-canvas = document.getElementById('canvas');
-ctx = canvas.getContext('2d');
-paint(ctx);
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+    run();
 } 
+
+function run(){
+    window.requestAnimationFrame(run);
+    act();
+    paint (ctx);
+}
+
+function act(){
+    x += 2;
+    if (x > canvas.width){
+        x=0;
+    }
+}
 
 window.addEventListener('load', init, false);
