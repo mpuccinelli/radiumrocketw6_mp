542f6f5941b1c9730f4ae8b50256340167be3e76
Mauricio Puccinelli <puccinellimauricio@gmail.com>
2020-11-10 08:47:21 -0300

    04 Interacting with other elemnts

--- game.js
+++ game.js
@@ -1,7 +1,6 @@
 var canvas = null,
     ctx = null,
-    x = 50,
-    y = 50,
+    player = null,
     lastPress = null,
     KEY_LEFT = 37,
     KEY_UP = 38,
@@ -9,14 +8,15 @@ var canvas = null,
     KEY_DOWN = 40,
     KEY_ENTER = 13,
     dir = 0,
-    pause = true;
+    pause = true,
+    score = 0;
 
 function paint(ctx) {
     ctx.fillStyle = '#000';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     
     ctx.fillStyle = '#0f0';
-    ctx.fillRect(x, y, 10, 10);
+    player.fill(ctx);
 
     //ctx.fillText('Last Press: ' + lastPress,0,20);
 
@@ -26,11 +26,19 @@ function paint(ctx) {
         ctx.fillText('PAUSE', 150, 75);
         ctx.textAlign = 'left';
     }
+
+    // Draw food
+    ctx.fillStyle = '#f00';
+    food.fill(ctx);
+
+    // Draw score
+    ctx.fillText('Score: ' + score, 0, 10);
 } 
 
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
+    player = new Rectangle(40, 40, 10, 10)
     run();
     repaint();
 } 
@@ -63,29 +71,29 @@ function act(){
 
         // Move Rect
         if (dir == 0) {
-            y -= 10;
+            player.y -= 10;
         }
         if (dir == 1) {
-            x += 10;
+            player.x += 10;
         }
         if (dir == 2) {
-            y += 10;
+            player.y += 10;
         }
         if (dir == 3) {
-            x -= 10;
+            player.x -= 10;
         }
         // Out Screen
-        if (x > canvas.width) {
-            x = 0;
+        if (player.x > canvas.width) {
+            player.x = 0;
         }
-        if (y > canvas.height) {
-            y = 0;
+        if (player.y > canvas.height) {
+            player.y = 0;
         }
-        if (x < 0) {
-            x = canvas.width;
+        if (player.x < 0) {
+            player.x = canvas.width;
         }
-        if (y < 0) {
-            y = canvas.height;
+        if (player.y < 0) {
+            player.y = canvas.height;
         }
     }
     // Pause/Unpause
@@ -93,9 +101,45 @@ function act(){
         pause = !pause;
         lastPress = null;
     }
+    // Food Intersects
+    if (player.intersects(food)) {
+        score += 1;
+        food.x = random(canvas.width / 10 - 1) * 10;
+        food.y = random(canvas.height / 10 - 1) * 10;
+    }
 }
 
 window.addEventListener('load', init, false);
 document.addEventListener('keydown',function(evt){
     lastPress = evt.which;
 },false)
+
+function Rectangle(x, y, width, height) {
+    this.x = (x == null) ? 0 : x;
+    this.y = (y == null) ? 0 : y;
+    this.width = (width == null) ? 0 : width;
+    this.height = (height == null) ? this.width : height;
+    this.intersects = function (rect) {
+        if (rect == null) {
+            window.console.warn('Missing parameters on function intersects');
+        } else {
+            return (this.x < rect.x + rect.width &&
+            this.x + this.width > rect.x &&
+            this.y < rect.y + rect.height &&
+            this.y + this.height > rect.y);
+        }
+    };
+    this.fill = function (ctx) {
+        if (ctx == null) {
+            window.console.warn('Missing parameters on function fill');
+        } else {
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    };
+}
+
+food = new Rectangle(80, 80, 10, 10);
+
+function random(max) {
+    return Math.floor(Math.random() * max);
+}
